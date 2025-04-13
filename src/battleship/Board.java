7b344d1749e5d7cfa60a93ee7a1a6f2683dcf822
Your Full Name <your_email@example.com>
2025-04-13 19:54:53 -0400

    Added complete Ship.java and Board.java

--- src/battleship/Board.java
+++ src/battleship/Board.java
@@ -3,27 +3,85 @@ package battleship;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board {
+public class Ship {
 	
-	private final Cell[][] grid; // 10x10 grid of cells
-	private final List<Ship> ships; // initialize list to store the ships
+	private final ShipType type;
+	private final List<int[]> coordinates;
 	
-	public Board()
+	public Ship(ShipType type, int startRow, int startCol, String orientation)
 	{
-		grid = new Cell[10][10];
-		ships = new ArrayList<>();
+		this.type = type;
+		this.coordinates = new ArrayList<>();
 		
-		// fill grid with new Cell objects (contain a cell, col)
-		
-		for (int row = 0; row < 10; row++)
+		switch (type)
 		{
-			for (int col = 0; col < 10; col++)
-			{
-				grid[row][col] = new Cell(row, col);
+		case DESTROYER:
+			// 2x2 square
+			coordinates.add(new int[] {startRow, startCol});
+			coordinates.add(new int[] {startRow, startCol + 1});
+			coordinates.add(new int[] {startRow + 1, startCol});
+			coordinates.add(new int[] {startRow + 1, startCol + 1});
+		
+		case SUBMARINE:
+			// diagonal 3 cells
+			if (orientation.equalsIgnoreCase("LR")) {
+                    coordinates.add(new int[]{startRow, startCol});
+                    coordinates.add(new int[]{startRow + 1, startCol + 1});
+                    coordinates.add(new int[]{startRow + 2, startCol + 2});
+                } else if (orientation.equalsIgnoreCase("RL")) {
+                    coordinates.add(new int[]{startRow, startCol});
+                    coordinates.add(new int[]{startRow + 1, startCol - 1});
+                    coordinates.add(new int[]{startRow + 2, startCol - 2});
+                }
+                break;
+
+			
+		case CRUISER:
+			// straight line 3 cells
+			if (orientation.equalsIgnoreCase("H")) {
+				coordinates.add(new int[]{startRow, startCol});
+				coordinates.add(new int[]{startRow, startCol + 1});
+				coordinates.add(new int[]{startRow, startCol + 2});
+			} else if (orientation.equalsIgnoreCase("V")) {
+				coordinates.add(new int[]{startRow, startCol});
+				coordinates.add(new int[]{startRow + 1, startCol});
+				coordinates.add(new int[]{startRow + 2, startCol});
 			}
-		}
+		
+			break;
+	}
+	 // Initialize hit tracker to all false
+	 for (int i = 0; i < coordinates.size(); i++) {
+		hitTracker.add(new boolean[]{false});
+	}
+}
+
+public ShipType getType() {
+	return type;
+}
+
+	public List<int[]> getCoordinates() 
+	{
+		return coordinates;
 	}
 	
-	
+	public boolean isHit(int row, int col) {
+        for (int i = 0; i < coordinates.size(); i++) {
+            int[] coord = coordinates.get(i);
+            if (coord[0] == row && coord[1] == col) {
+                hitTracker.get(i)[0] = true;
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean isSunk() {
+        for (boolean[] hit : hitTracker) {
+            if (!hit[0]) return false;
+        }
+        return true;
+    }
 }
+
+
--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -1,5 +1,6 @@
 package battleship;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
@@ -23,20 +24,64 @@ public class Ship {
 		
 		case SUBMARINE:
 			// diagonal 3 cells
+			if (orientation.equalsIgnoreCase("LR")) {
+                    coordinates.add(new int[]{startRow, startCol});
+                    coordinates.add(new int[]{startRow + 1, startCol + 1});
+                    coordinates.add(new int[]{startRow + 2, startCol + 2});
+                } else if (orientation.equalsIgnoreCase("RL")) {
+                    coordinates.add(new int[]{startRow, startCol});
+                    coordinates.add(new int[]{startRow + 1, startCol - 1});
+                    coordinates.add(new int[]{startRow + 2, startCol - 2});
+                }
+                break;
+
 			
 		case CRUISER:
 			// straight line 3 cells
-		}
-		break;
+			if (orientation.equalsIgnoreCase("H")) {
+				coordinates.add(new int[]{startRow, startCol});
+				coordinates.add(new int[]{startRow, startCol + 1});
+				coordinates.add(new int[]{startRow, startCol + 2});
+			} else if (orientation.equalsIgnoreCase("V")) {
+				coordinates.add(new int[]{startRow, startCol});
+				coordinates.add(new int[]{startRow + 1, startCol});
+				coordinates.add(new int[]{startRow + 2, startCol});
+			}
+		
+			break;
 	}
-	
+	 // Initialize hit tracker to all false
+	 for (int i = 0; i < coordinates.size(); i++) {
+		hitTracker.add(new boolean[]{false});
+	}
+}
+
+public ShipType getType() {
+	return type;
+}
+
 	public List<int[]> getCoordinates() 
 	{
 		return coordinates;
 	}
 	
-	
+	public boolean isHit(int row, int col) {
+        for (int i = 0; i < coordinates.size(); i++) {
+            int[] coord = coordinates.get(i);
+            if (coord[0] == row && coord[1] == col) {
+                hitTracker.get(i)[0] = true;
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean isSunk() {
+        for (boolean[] hit : hitTracker) {
+            if (!hit[0]) return false;
+        }
+        return true;
+    }
 }
 
 
